07145e79810eef6fccf8ff057203c268f978dfa2
Priyanka Mane Patil <mane.priyanka39@gmail.com>
2025-12-07 22:26:31 +0530

    Update deploy_to_space.py

--- src/deploy_to_space.py
+++ src/deploy_to_space.py
@@ -1,13 +1,25 @@
-from huggingface_hub import upload_folder
+import os
+from huggingface_hub import upload_folder, HfApi
 
-SPACE_REPO = "priyankamane10/visitwithus-tourism-app"
-
-upload_folder(
-    folder_path="app",
-    repo_id=SPACE_REPO,
-    repo_type="space",
-    path_in_repo=".",
-    commit_message="Deploying Streamlit App via Automation"
+SPACE_REPO_ID = os.environ.get(
+    "HF_SPACE",
+    "priyankamane10/visitwithus-tourism-app"  # fallback
 )
 
-print("Deployment completed.")
+def deploy():
+    api = HfApi()
+
+    print(f"Deploying app folder to Hugging Face Space: {SPACE_REPO_ID}")
+
+    upload_folder(
+        folder_path="app",
+        repo_id=SPACE_REPO_ID,
+        repo_type="space",
+        path_in_repo=".",  # root of space
+        commit_message="Automated deployment from GitHub Actions CI/CD pipeline"
+    )
+
+    print("Deployment successful.")
+
+if __name__ == "__main__":
+    deploy()
